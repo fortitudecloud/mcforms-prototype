83949bc24f289407221b4dc183e52d7030062292
Hickey, Lionel <Lionel.Hickey@sew.com.au>
2017-11-13 16:34:25 +1100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule, BrowserAnimationsModule, FormsModule,
+        MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
